6a436c068d082036f9e2c79da72df0064c2bcd7d
todddrinkwater <todddrinkwaternz@gmail.com>
2017-08-09 11:45:10 +1200

    added a search bar able to choose what you want to search

---
+++ client/components/Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Form (props){
+  return (
+    <div>
+      <form>
+        Subredit type: <input type='text' name='stuff' />
+      </form>
+    </div>
+  )
+}
+
+
+export default Form
--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -2,10 +2,33 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {fetchPosts} from '../actions'
 
-let LoadSubreddit = ({dispatch}) => (
-  <button
-    onClick={() => dispatch(fetchPosts('newzealand'))}
-    >Fetch Posts</button>
-)
+class LoadSubreddit extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      subreddit:""
+    }
+  }
+  handleChange(e){
+    this.setState({
+      subreddit: e.target.value
+    })
+  }
+  render() {
+    const {dispatch} = this.props
+    console.log(this.props);
+    return(
+      <div>
+        <div>
+          Subredit type:  <input onChange={this.handleChange.bind(this)}
+            value={this.state.subreddit} type='text' name='stuff' />
+      </div>
+        <button
+          onClick={() => dispatch(fetchPosts(this.state.subreddit))}
+          >Fetch Posts</button>
+      </div>
+    )
+  }
+}
 
 export default connect()(LoadSubreddit)
